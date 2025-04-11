2f7c34a1ec676b0eda011ecddbd199c2f70f0e44
Abha Meshram <abha25meshram@gmail.com>
2025-04-11 20:09:11 +0530

    added api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", 
+  baseURL: "https://student-job-tracker-d3qj.onrender.com", 
 });
 
 export const fetchJobs = () => API.get("/job");
